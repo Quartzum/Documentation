3a0fa4c5337f2193538f66350f4018529de5f3c5
Viktor <Tempert@yandex.ru>
2022-09-30 04:15:06 +0300

    Add Example 2, 3, 4, 5

---
+++ Geekbrains/Examples/Example003_UserFavorite/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "viktor")
+{
+    Console.WriteLine("Ура, это же Viktor!");
+}
+else
+{
+    Console.WriteLine("Привет");
+    Console.WriteLine(userName);
+}
---
+++ Geekbrains/Examples/Example004_Weights/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 10;
+int b = 7;
+int c = 9;
+int e = 5;
+int z = 0;
+
+int max = z;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(e > max) max = e;
+
+Console.Write("Максимальное число равно = ");
+Console.WriteLine(max);
