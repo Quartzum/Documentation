7f872cd61af1c130c4545941cb051553942118c6
Viktor <Tempert@yandex.ru>
2022-10-05 22:48:58 +0300

    Added folder Seminar 2 in folder Homeworks. Created folder task_1

---
+++ Geekbrains/Acquaintance_with_languages/Homework/Seminar_2/task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Start");
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = num / 10 % 10;
+
+if(result < 0){
+    result = result * -1;
+    Console.WriteLine($"Второе число {num} : {result}.");
+} else{
+    Console.WriteLine($"Второе число {num} : {result}.");
+}
