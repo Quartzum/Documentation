6a09d972d001d189f10503f472f8aae481e2cd53
Viktor <Tempert@yandex.ru>
2022-10-08 19:03:11 +0300

    Added some infromation

--- Geekbrains/Acquaintance_with_languages/Homework/Seminar_2/task_1/Program.cs
+++ Geekbrains/Acquaintance_with_languages/Homework/Seminar_2/task_1/Program.cs
@@ -1,12 +1,13 @@
-﻿Console.WriteLine("Start");
+﻿Console.Clear();
+Console.WriteLine("Start");
 Console.Write("Введите трехзначное число: ");
 int num = int.Parse(Console.ReadLine());
 
 int result = num / 10 % 10;
 
-if(result < 0){
-    result = result * -1;
+if(num > 0){
     Console.WriteLine($"Второе число {num} : {result}.");
-} else{
+} else {
+    result = result * -1;
     Console.WriteLine($"Второе число {num} : {result}.");
 }
