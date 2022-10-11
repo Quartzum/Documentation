7795ace631972911a358082beb0b93fe943b5e73
Viktor <Tempert@yandex.ru>
2022-10-11 16:43:49 +0300

    Added folders - tasks and Seminar_2. Completed three tasks from homework.

--- Geekbrains/Acquaintance_with_languages/Homework/Seminar_2/task_1/Program.cs
+++ Geekbrains/Acquaintance_with_languages/Homework/Seminar_2/task_1/Program.cs
@@ -5,9 +5,11 @@ int num = int.Parse(Console.ReadLine());
 
 int result = num / 10 % 10;
 
-if(num > 0){
-    Console.WriteLine($"Второе число {num} : {result}.");
-} else {
+if(num > 99 && num < 1000){
+    Console.WriteLine($"Второе число {num} - {result} .");
+} else if(num < -99 && num > -1000) {
     result = result * -1;
-    Console.WriteLine($"Второе число {num} : {result}.");
+    Console.WriteLine($"Второе число {num} - {result} .");
+} else {
+    Console.WriteLine($"{num} - некорректное число!Введите трехзначное число в консоль!");
 }
