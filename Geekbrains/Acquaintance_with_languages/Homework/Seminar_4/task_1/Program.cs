aee85bc8046b5849180cad0c784d09be0d22cf1c
Viktor <Tempert@yandex.ru>
2022-10-15 00:18:57 +0300

    Added Seminar_5

--- Geekbrains/Acquaintance_with_languages/Homework/Seminar_4/task_1/Program.cs
+++ Geekbrains/Acquaintance_with_languages/Homework/Seminar_4/task_1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Start");
+
+    int[]array = new int[2];
+
+    FillArray(array);
+
+    Console.WriteLine($"{array[0]} в степени {array[1]} - {Exponentiation(array)}.");
+   
+    int GetNumber()
+    {
+        Console.Write("Введите число: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    int[]FillArray(int[]arg1)
+    {
+        arg1[0] = GetNumber();
+        arg1[1] = GetNumber();
+        return arg1;
+    }
+
+    int Exponentiation(int[]arg1)
+    {
+        int result = 1;
+        
+        while(arg1[1] > 0)
+        {
+            result *= arg1[0];
+            arg1[1]--;
+        } 
+    
+    return result;
+    }
+
+    Console.WriteLine("End");
+}
+
+Main();
---
+++ Geekbrains/Acquaintance_with_languages/Seminars_C#/Seminar_5/task_5/Program.cs
@@ -0,0 +1,52 @@
+﻿void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Start");
+    int[]FillArray = CreateArray(EnterSizeArray(), EnterMinNumber(), EnterMaxNumber());
+    Console.Write(string.Join(" ", FillArray));
+    Console.WriteLine($"{Composition(FillArray)}");
+
+
+    Console.WriteLine("End");
+}
+int EnterSizeArray()
+{
+    Console.Write("Введите размер массива: ");
+    return int.Parse(Console.ReadLine());
+}
+int EnterMinNumber()
+{
+    Console.Write("Введите минимальное значение искомого диапазона: ");
+    return int.Parse(Console.ReadLine());
+}
+int EnterMaxNumber()
+{
+    Console.Write("Введите максимальное значение искомого диапазона: ");
+    return int.Parse(Console.ReadLine());
+}
+int [] CreateArray(int arg1, int arg2, int arg3)
+{
+    int[]array = new int[arg1];
+    for (int i = 0; i < arg1; i++)
+    {
+        array[i] = new Random().Next(arg2, arg3);
+    }
+    return array;
+}
+int Composition(int[]arg1)
+{
+    int CountStart = 0;
+    int CountEnd = arg1.Length-1;
+    int result = 0;
+    while (CountStart == CountEnd)
+    {
+        result = arg1[CountStart] * arg1[CountEnd];
+        CountStart++;
+        CountEnd--;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"{CountEnd}");
+    Console.WriteLine($"{CountStart}");
+    return result;
+}
+Main();
