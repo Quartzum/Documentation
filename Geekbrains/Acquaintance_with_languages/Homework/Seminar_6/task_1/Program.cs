60052ff0563d4033593dab0c8c7d045356c3919c
Viktor <Tempert@yandex.ru>
2022-10-24 19:39:41 +0300

    Completed task_1 and task_2

--- Geekbrains/Acquaintance_with_languages/Homework/Seminar_6/task_1/Program.cs
+++ Geekbrains/Acquaintance_with_languages/Homework/Seminar_6/task_1/Program.cs
@@ -1,34 +1,24 @@
-﻿/*
-## Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-
-0, 7, 8, -2, -2 -> 2
-
-1, -7, 567, 89, 223-> 3
-*/
-
-void Main()
+﻿void Main()
 {
     Console.Clear();
     Console.WriteLine("Start");
-
-    Console.WriteLine("End");
+    Console.Write("Введите элементы(через пробел): ");
+    CheckNumber();
+    Console.Write("End");
 }
 
-int[]CalculateUserReadNumber()
+void CheckNumber()
 {
-    int i = 0;
-    int[]temp = new int[i];
-    string empty = "";
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int count = 0;
 
-    while(true)
+    for (int i = 0; i < arr.Length; i++)
     {
-        Console.WriteLine("Enter number: ");
-        temp[i] = int.Parse(Console.ReadLine());
-        Console.WriteLine("Contunue enter numbers? Yes/No.");
-        empty = Console.ReadLine();
-        if(empty == "No" || empty == "no" || empty = "n")return;
-        
+        if (arr[i] > 0)
+        {
+            count++;
+        }
     }
-    
-    return temp;
+    Console.WriteLine($"Кол-во элементов > 0: {count}.");
 }
+Main();
--- Geekbrains/Acquaintance_with_languages/Homework/Seminar_6/task_2/Program.cs
+++ Geekbrains/Acquaintance_with_languages/Homework/Seminar_6/task_2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Start");
+    CheckIntersection(GetNumber(), GetNumber(), GetNumber(), GetNumber());
+    Console.WriteLine("End");
+}
+double GetNumber()
+{
+    Console.Write("Введите координату: ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+void CheckIntersection(double b1, double k1, double b2, double k2)
+{
+    double x = -(b1-b2)/(k1-k2);
+    double y = k1 * x + b1;
+
+    x = Math.Round(x, 3);
+    y = Math.Round(y, 3);
+
+    Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
+Main();
