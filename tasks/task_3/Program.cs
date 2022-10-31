40b801df251f55c9b48e9b3df49ef4f3da729f88
Viktor <Tempert@yandex.ru>
2022-10-31 23:21:18 +0300

    Completed task_3 - Binary massiv

--- tasks/task_3/Program.cs
+++ tasks/task_3/Program.cs
@@ -1,1 +1,31 @@
-﻿int [] Array = new int [5];
+﻿/* Найдите максимальное число в отсортированном одномерном массиве*/
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Start");
+    int[]array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int searching = GetNumber();
+    Console.WriteLine($"Индекс искомого числа: {searching} - {BinaryAlgoritmh(array, searching)}.");
+    Console.WriteLine("End");
+}
+int GetNumber()
+{
+    Console.Write("Введите искомое число массива: ");
+    return int.Parse(Console.ReadLine());
+}
+int BinaryAlgoritmh(int[]array, int searching)
+{
+    int min = 0;
+    int max = array.Length-1;
+    
+    while(min <= max)
+    {
+        int mid = (min + max) / 2;
+        if(searching == array[mid])return mid;
+        if(searching < array[mid])max = mid - 1;
+        else min = mid + 1;
+    }
+    return -1;
+}
+Main();
