8e34331b980e3cb8fe81c79e6bac58d026f587a0
Victor Popov <Tempert@yandex.ru>
2023-01-17 00:20:49 +0300

    Completed Homework_5 in Introduction in Python

---
+++ Geekbrains/Introduction in Python/Homework/Homework_5/task_1/task.py
@@ -0,0 +1,16 @@
+with open('incoming_text.txt','w', encoding='UTF-8' ) as file:
+    file.write(input('Введите первоночальный текст: '))
+with open('incoming_text.txt','r') as file:
+    my_text = file.readline()
+    change_text = my_text.split()
+print()
+print(my_text)
+print()
+
+del_text = input('Введите  набор букв, который нужно удалить из слов содержащие данную последовательность :  ')
+print(del_text)
+
+result = ' '.join(filter(lambda x: del_text not in x, change_text))
+with open('format_text.txt','w', encoding='UTF-8') as file:
+    file.write(f'{result}')
+print(result)
