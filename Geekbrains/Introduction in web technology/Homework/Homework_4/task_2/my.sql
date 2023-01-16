b9c89b68c0c3ab5e2c04ccbbcedd22d0622233ae
Victor Popov <Tempert@yandex.ru>
2023-01-17 00:00:50 +0300

    Completed Homework_4 in Introduction in web technology

--- Geekbrains/Introduction in web technology/Homework/Homework_4/task_2/my.sql
+++ Geekbrains/Introduction in web technology/Homework/Homework_4/task_2/my.sql
@@ -1,1 +1,15 @@
-SELECT
+CREATE TABLE classmates(
+    classmates_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30),
+    age INT,
+    address VARCHAR(200)
+);
+
+INSERT INTO classmates (classmates_id, name, age, address) VALUES
+    (1, 'Victor', 26, 'Moscow'),
+    (2, 'Anna', 18, 'Moscow'),
+    (3, 'Michail', 29, 'Moscow'),
+    (4, 'Olga', 34, 'Onega'),
+    (5, 'Anton', 60, 'Moscow');
+
+SELECT name FROM classmates WHERE address = 'Moscow' AND age BETWEEN 18 AND 29;
